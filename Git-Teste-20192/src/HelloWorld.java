a7b3bebdc69c0ac6b9b55748fc3d93953c7f96f9
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2019-10-01 15:49:11 -0300

    Mudado print bash

--- Git-Teste-20192/src/HelloWorld.java
+++ Git-Teste-20192/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!");
 		
-		System.out.println("BASH");
+		System.out.println("BASH !!!");
 		
 		System.out.println("Eclipse");
 	}
